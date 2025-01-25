4951899b4edf31b8f315379809a6a06670aa004a
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-01-25 09:55:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    res.render('homePage');
+})
+
+app.listen(PORT, ()=>{console.log(`Running at http://localhost:3000`)});
