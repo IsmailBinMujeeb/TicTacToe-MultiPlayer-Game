bb5376d47a397681b35fd69c65a9f25abfe7044d
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-01-28 12:38:44 +0530

    bug/getting hashed password in user api

--- controllers/apiCountrollers.js
+++ controllers/apiCountrollers.js
@@ -4,7 +4,7 @@ const roomModel = require('../models/room-model');
 const usersApiRout = async (req, res)=>{
     const regExpUsername = new RegExp(`^${req.params.username}$`, 'i');
 
-    const user = await userModel.findOne({ username: regExpUsername });
+    const user = await userModel.findOne({ username: regExpUsername }).select('-password');
 
     if(!user) return res.status(404).json({ errorCode: 404,  message: 'User not found!' });
 
